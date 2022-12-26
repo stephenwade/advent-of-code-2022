7f2df0274dc30344c49cf5406cc72a5d48500bab
Stephen Wade <stephen@stephenwade.me>
2022-12-26 03:51:14 -0500

    day 5

---
+++ day-05/part-2.ts
@@ -0,0 +1,70 @@
+#!/usr/bin/env node -r @swc-node/register
+
+import * as assert from 'node:assert';
+
+import { readInput } from '~/util/read-input';
+
+const inputText = readInput(__dirname);
+
+const [stacksText, instructionsText] = inputText.split('\n\n');
+
+type Stacks = Record<string, string[]>;
+
+function parseStacks(input: string): Stacks {
+  const stacksLines = input.split('\n');
+  const stacksCount = (stacksLines[0].length + 1) / 4;
+
+  const stackArrays: string[][] = [];
+  for (let i = 0; i < stacksCount; i += 1) {
+    stackArrays.push([]);
+  }
+
+  for (const line of stacksLines) {
+    for (let i = 0; i < stacksCount; i += 1) {
+      const item = line.slice(i * 4 + 1, i * 4 + 2);
+      stackArrays[i].push(item);
+    }
+  }
+
+  const result: Stacks = {};
+
+  for (const stackArray of stackArrays) {
+    const label = stackArray.pop();
+    assert(label);
+
+    result[label] = stackArray.filter((x) => x !== ' ').reverse();
+  }
+
+  return result;
+}
+
+const stacks = parseStacks(stacksText);
+
+type Instruction = {
+  move: number;
+  from: string;
+  to: string;
+};
+
+function parseInstruction(input: string): Instruction {
+  const matchResult = input.match(/move (\d+) from (\w+) to (\w+)/u);
+  assert(matchResult);
+
+  const [, moveStr, from, to] = matchResult;
+  return { move: Number(moveStr), from, to };
+}
+
+const instructions = instructionsText.split('\n').map(parseInstruction);
+
+for (const { move, from, to } of instructions) {
+  const crates = stacks[from].slice(-move);
+
+  stacks[from] = stacks[from].slice(0, -move);
+  stacks[to].push(...crates);
+}
+
+console.log(
+  Object.values(stacks)
+    .map((stack) => stack.pop())
+    .join('')
+);
